deeea0ce72053a23efcc53421215be54a0ba1e1d
David Kügler <david.kuegler@gris.informatik.tu-darmstadt.de>
2017-02-21 17:29:38 +0100

    update InvokeLater --> condense into one template

--- include/gstd/InvokeLater.h
+++ include/gstd/InvokeLater.h
@@ -31,56 +31,49 @@ private:
 };
 
 
-template<typename... Args>
-class InvokeLaterQueue
+template<typename Container, typename... Args>
+class InvokeLaterTemplate
 {
 public:
   typedef std::function<Args...> Functor;
-  InvokeLaterQueue() { }
-  InvokeLaterQueue(const Functor& f) { push(f); }
-  InvokeLaterQueue(const InvokeLaterQueue& c) : q(c.q) {};
-  ~InvokeLaterQueue() { while (!q.empty()) { invoke(); } }
-  void push(const Functor& f) { q.push(f); }
-  inline void invoke() { try{ q.front()(); } catch (...) {} q.pop(); }
-  void remove() { q.pop(); }
-  void clear() { while (!q.empty()) q.pop(); }
+  InvokeLaterTemplate() { }
+  InvokeLaterTemplate(const Functor& f) { push(f); }
+  InvokeLaterTemplate(const InvokeLaterTemplate& r) : c(r.c) {};
+  ~InvokeLaterTemplate() { while (!c.empty()) { invoke(); } }
+  void push(const Functor& f) { c.push(f); }
+  inline void invoke() { try{ next()(); } catch (...) {} c.pop(); }
+  void remove() { c.pop(); }
+  void clear() { while (!c.empty()) c.pop(); }
 private:
-  InvokeLaterQueue& operator=(const InvokeLaterQueue& c) = delete;
-  std::queue<Functor> q;
+  InvokeLaterTemplate& operator=(const InvokeLaterTemplate& c) = delete;
+  Container<Functor> c;
 };
 
-template<typename... Args>
-class InvokeLaterStack
-{
-public:
-  typedef std::function<Args...> Functor;
-  InvokeLaterStack() { }
-  InvokeLaterStack(const Functor& f) { push(f); }
-  InvokeLaterStack(const InvokeLaterStack& c) : s(c.s) {};
-  ~InvokeLaterStack() { while (!s.empty()) { invoke(); } }
-  void push(const Functor& f) { s.push(f); }
-  inline void invoke() { try { s.top()(); } catch (...) {} s.pop(); }
-  void remove() { s.pop(); }
-  void clear() { while (!s.empty()) s.pop(); }
-private:
-  InvokeLaterStack& operator=(const InvokeLaterStack& c) = delete;
-  std::stack<Functor> s;
-};
+template<typename...Args>
+class InvokeLaterTemplate<std::queue, Args...>
+{  protected: Functor& next() { return front(); } };
 
-template<typename... Args>
+template<typename...Args>
+class InvokeLaterTemplate<std::stack, Args...>
+{  protected: Functor& next() { return top(); } };
+
+template<typename...Args> typedef InvokeLaterTemplate<std::queue, Args...> InvokeLaterQueue;
+template<typename...Args> typedef InvokeLaterTemplate<std::stack, Args...> InvokeLaterStack;
+
+template<typename Key, typename... Args>
 class InvokeLaterList
 {
 public:
   typedef std::function<Args...> Functor;
-  typedef std::map<int, Functor> FunctorMap;
-  typedef std::pair<int, Functor> FunctorPair;
+  typedef std::map<Key, Functor> FunctorMap;
+  typedef std::pair<Key, Functor> FunctorPair;
   enum enInvocationOrder { UNDEFINED, FIFO, LIFO };
-  InvokeLaterList(const enInvocationOrder io = LIFO) : nIdx(1), invoOrder(io) {}
+  InvokeLaterList(const enInvocationOrder io = LIFO) : invoOrder(io) {}
   InvokeLaterList(const Functor& f, const enInvocationOrder io = LIFO) : InvokeLater(io) { push(f); }
   ~InvokeLaterList() { while (!m.empty()) { invoke(); } }
-  int push(const Functor& f) { m.insert(m.end(), FunctorPair(nIdx, f)); return nIdx++; }
-  void remove(const int idx) { auto it = m.find(idx); remove(it); }
-  inline void invoke(const int idx) {  auto it = m.find(idx); invoke(it); }
+  bool push(const Key& idx, const const Functor& f) { if (m.find(idx) == m.end()) { m.insert(m.end(), FunctorPair(idx, f)); return true; } return false; }
+  void remove(const Key idx) { auto it = m.find(idx); remove(it); }
+  inline void invoke(const Key idx) {  auto it = m.find(idx); invoke(it); }
   void clear() { q.clear(); }
   void setInvocationOrder(const enInvocationOrder io) { invoOrder = io; }
   enInvocationOrder getInvocationOrder() const { return invoOrder; }
@@ -91,8 +84,16 @@ private:
   InvokeLaterList(const InvokeLaterList& c) = delete;
   InvokeLaterList& operator=(const InvokeLaterList& c) = delete;
   FunctorMap m;
-  int nIdx;
   enInvocationOrder invoOrder;
+};
+
+template<typename... Args>
+class InvokeLaterList<int, Args...>
+{
+  InvokeLaterList(const enInvocationOrder io = LIFO) : nIdx(1), invoOrder(io) {}
+  Key push(const Functor& f) { while (!push(nIdx, f)) ++nIdx; return nIdx - 1; }
+private:
+  int nIdx;
 };
 
 }
