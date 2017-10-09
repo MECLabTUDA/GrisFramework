4dc4f1d30e6a0ceecdc53c4fb63b82f42fcd6368
Johannes Fauser <johannes.fauser@gris.tu-darmstadt.de>
2017-10-09 09:04:57 +0200

    B#8407: fixed Bug (XxmlNode.setValue(..)), todo: Unit test vernünftig schreiben

--- src/tests/testXml/testXml.cpp
+++ src/tests/testXml/testXml.cpp
@@ -13,24 +13,40 @@ int main(int argc, char ** argv)
   using namespace std;
   try
   {
+    const std::string fn = "XmlDocumentText.xml";
+    const std::string root = "root";
+    const std::string child1 = "child1";
+    const std::string text1 = "text1";
+    const std::string text2 = "text2";
+
     {
-      auto doc = XmlDocument::read("testReadXml.xml");
+      auto doc = XmlDocument::create(root.c_str());
+      auto root = doc->getRoot();
+      root.addChild(child1.c_str()).setValue(text1.c_str());
+      XmlDocument::save(fn.c_str(), *doc);
+    }
+    {
+      auto doc = XmlDocument::read(fn.c_str());
+      auto root = doc->getRoot();
+      auto child = root.getChild(child1.c_str());
+      std::cout << child.getValue() << std::endl;
+      child.setValue(text2.c_str());
+      XmlDocument::save(fn.c_str(), *doc);
+    }
+    {
+      auto doc = XmlDocument::read(fn.c_str());
+      auto root = doc->getRoot();
+      auto child = root.getChild(child1.c_str());
+      std::cout << child.getValue() << std::endl;
+    }
+    {
+      /*auto doc = XmlDocument::read("testReadXml.xml");
       auto root = XmlNode(doc->getRoot());
       auto att = root.getChild("ApplicationSettings").getAttribute("option_a");
       std::cout << att << endl;
       root.getChild("ApplicationSettings").getAttribute("option_a").setValue("blubb");
       std::cout << att << endl;
-      XmlDocument::save("testSaveXml.xml", *doc);
-    }
-
-    {
-      auto doc = gris::XmlDocument::create("scene");
-      gris::XmlNode root = doc->getRoot();
-      gris::XmlNode node = root.addChild("Calibration");
-      node.setValue("test");
-      std::cout << "Get: " << node.getValue() << endl;
-      gris::XmlNode obs  = root.addChild("Obstacles");
-      gris::XmlDocument::save("scene.xml", *doc);
+      XmlDocument::save("testSaveXml.xml", *doc);*/
     }
   }
   catch (std::exception& e)
